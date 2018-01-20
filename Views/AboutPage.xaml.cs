55fb7824183fa776c81bb0e63562a072934d03e5
SiyaoX <siyaoxu817@gmail.com>
2018-01-19 17:29:18 -0800

    Initial check-in of module TheLastHero

---
+++ Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TheLastHero
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
